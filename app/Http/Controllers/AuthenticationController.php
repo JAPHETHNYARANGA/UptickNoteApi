00c139f413aee5112b65435c8fda3a0c4e86ca9e
JAPHETHNYARANGA <nyaranga4@gmail.com>
2023-02-16 08:32:34 +0300

    Created the register controller and the register api route

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthenticationController extends Controller
 {
-    //
+    public function register(Request $request){
+        $request->validate([
+            'name'=>'required',
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+        
+        $user= new User();
+        $user ->name = $request->name;
+        $user ->email = $request->email;
+        $user ->password = $request->password;
+
+        $res = $user->save();
+
+        if($res){
+            return response([
+               'success' =>true,
+               'message' =>'user registered successfully',
+               'user'=>$user 
+            ],200);
+        }else{
+            return response([
+                'success'=>false,
+                'message'=>'user register failed',
+            ],201);
+        }
+    }
 }
