515e0b00d9e502d7547e04142dc3df29253d2174
JAPHETHNYARANGA <nyaranga4@gmail.com>
2023-02-16 09:51:28 +0300

    Passport token created when a user logs in

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -45,7 +45,7 @@ class AuthenticationController extends Controller
         $email = $request->email;
         $user = User::where('email', $email)->firstOrFail();
         
-        // $token = $user->createToken('Api Token')->accessToken;
+        $token = $user->createToken('Api Token')->accessToken;
         
         $credentials = $request->only('email', 'password');
         if(Auth::attempt($credentials)){
@@ -53,7 +53,7 @@ class AuthenticationController extends Controller
                 [
                     'success'=>true,
                     'user'=>$user,
-                    // 'token'=>$token
+                    'token'=>$token
                 ],200);
         }
         return response([
