cc05a9f4e1509cf33b795f2e1f0767bbdb27cd04
JAPHETHNYARANGA <nyaranga4@gmail.com>
2023-02-20 11:31:37 +0300

    Login test still failed

--- tests/Unit/AuthenticationControllerTest.php
+++ tests/Unit/AuthenticationControllerTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Passport\Passport;
 
@@ -14,26 +15,7 @@ class AuthenticationControllerTest extends TestCase
     use RefreshDatabase;
 
 
-    public function testLogin()
-    {
-
-
-        $token = Passport::actingAs(
-            User::factory()->create(),
-            ['create-servers']
-        );
-
-        $response = $this->withHeaders([
-            'Authorization' => 'Bearer ' . $token,
-        ])->post('/api/login', [
-            'email' => 'nyaranga4@gmail.com',
-            'password' => '123456',
-        ]);
-
-        $response->assertStatus(200);
-
-      
-    }
+   
 
     public function testRegister()
     {
@@ -45,5 +27,30 @@ class AuthenticationControllerTest extends TestCase
         ]);
 
         $response->assertStatus(200);
+    }
+
+    public function testLogin()
+    {
+       
+
+        $user = User::create([
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        $email = $user->email;
+        $user = User::where('email', $email)->firstOrFail();
+        $token = $user->createToken('Api Token')->accessToken;
+
+        $response = $this->post('/api/login', [
+            'email' => 'johndoe@example.com',
+            'password' => 'password',
+            'token' => $token
+        ]);
+
+        $response->assertStatus(200);
+
+      
     }
 }
